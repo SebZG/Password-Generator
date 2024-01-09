3aaa4d34c0798f7eb2e349f5e8748d142e271930
SebZG <sebastian.zapata.g@gmail.com>
2024-01-09 17:45:08 +0000

    Improving readability

--- js/generatePassword.js
+++ js/generatePassword.js
@@ -6,16 +6,17 @@ const getRandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
 // Generate password based on users options
 const generatePassword = (length, selectedOptions) =>
   Array.from({ length }, () => getRandomElement(selectedOptions)).join('');
-
+                                                                                                                      
 // Write password to the #password input
 const writePassword = () => {
   const passwordOptions = getPasswordOptions();
   if (!passwordOptions) return; // Check if password generation was canceled
   const selectedOptions = getSelectedOptions(passwordOptions, options);
-  console.log("Password length: " + passwordOptions.length);
   const password = generatePassword(passwordOptions.length, selectedOptions);
-  console.log("Password: " + password);
   const passwordText = document.querySelector('#password');
+  
+  console.log("Password length: " + passwordOptions.length);
+  console.log("Password: " + password);
 
   passwordText.value = password;
   passwordText.focus();
